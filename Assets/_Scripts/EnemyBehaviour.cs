7b3ea6b5666339d05828672958ea0893c15e5f29
A. Mitchell <mitchelladam13@yahoo.ca>
2021-03-08 22:46:07 -0500

    Modified Enemy Behaviour

--- Assets/_Scripts/EnemyBehaviour.cs
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -7,13 +7,23 @@ public class EnemyBehaviour : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public Transform player;
-
+    
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
 
+
+            Destroy(this.gameObject);
+
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
